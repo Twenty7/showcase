678f4e659c8fc1578acb0547cfc6005c60f84166
Matt Taylor <mtaylor@illuminateed.com>
2019-01-17 17:45:10 -0800

    Add Font-Awesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,10 @@
 // any CSS you require will output into a single css file (app.scss in this case)
 require('../css/app.scss');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// Init Libraries
 const $ = require('jquery');
 require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
