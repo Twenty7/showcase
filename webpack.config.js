d1b06c986f142a560e1ec614bc6abb2810ff9b3c
Matt Taylor <mtaylor@illuminateed.com>
2019-01-17 23:52:40 -0800

    Add React JS

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ Encore
     .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
-    //.enableReactPreset()
+    .enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
 ;
 
